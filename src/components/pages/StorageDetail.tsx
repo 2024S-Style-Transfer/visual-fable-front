3dac6828c231dc1e2f7f99fc197119d36ff14af8
Climier-code <ikowhddus@naver.com>
2024-05-27 00:36:30 +0900

    feat: storage detail page 추가

---
+++ src/components/pages/StorageDetail.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import DoneStep from '../steps/DoneStep';
+import { GeneratedItem } from '@/types/service';
+import { MOCK_GENERATED_ITEMS } from '@/mock/data';
+import useGlobalStore from '@/store/globalStore';
+
+type Props = {
+  id: string;
+};
+const StorageDetail: React.FC<Props> = ({ id }) => {
+  const { isLogin } = useGlobalStore();
+
+  const [generatedItems, setGeneratedItems] = useState<GeneratedItem[]>([]);
+
+  // FIXME: API 호출로 대체
+  const loadGeneratedItems = async () => {
+    console.log(id);
+    setGeneratedItems(MOCK_GENERATED_ITEMS);
+  };
+
+  useEffect(() => {
+    if (!isLogin) {
+      alert('로그인이 필요합니다.');
+      window.location.href = '/';
+    }
+
+    loadGeneratedItems();
+  }, []);
+
+  return <DoneStep generatedItems={generatedItems} />;
+};
+
+export default StorageDetail;

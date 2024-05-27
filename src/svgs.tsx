2ac4eeedfafa7ff7dfbaef77dd1043d1975b0a0f
Climier-code <ikowhddus@naver.com>
2024-05-27 14:50:06 +0900

    chore: add SvgChevronDown

--- src/svgs.tsx
+++ src/svgs.tsx
@@ -75,3 +75,12 @@ export const SvgLockIcon = () => (
     </defs>
   </svg>
 );
+
+export const SvgChevronDown = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+    <path
+      fillRule="evenodd"
+      d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
+    />
+  </svg>
+);

2f5de5d53fae55ab37cecb7e22e2cf3b99e360c5
LSY <kongma0312@gmail.com>
2024-05-29 06:05:15 +0900

    style: ColorTheme 기본색상, 보조 색상 변경

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,10 +1,10 @@
 // 컬러 팔레트
 const ColorTheme = {
   backColor: '#ffffff', //배경 색상 - 흰색
-  primaryColor: '#FF8A00', //기본 색상 - 주황
+  primaryColor: '#FFA336', //기본 색상 - 주황
   primaryColor2: '#E37800', //기본 색상2 - 주황
   primaryColor3: '#C76900', //기본 색상3 - 주황
-  secondaryColor: '#001AFF', //보조 색상 - 파랑
+  secondaryColor: '#253BFF', //보조 색상 - 파랑
   secondaryColor2: '#0B1FD5',
   secondaryColor3: '#0E1FB8',
   negativeColor: '#979797', //부정 색상 - 회색 (취소 버튼)

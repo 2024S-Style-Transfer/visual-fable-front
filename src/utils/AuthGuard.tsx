7a2886e4af4f9f3f43ae52f8dc1ed3c161ba345c
Climier-code <ikowhddus@naver.com>
2024-05-31 22:23:30 +0900

    fix: `AuthGuard`의 로그인 체크 로직 수정

--- src/utils/AuthGuard.tsx
+++ src/utils/AuthGuard.tsx
@@ -7,22 +7,41 @@ import { Fragment, useEffect } from 'react';
 const AuthGuard: React.FC<React.PropsWithChildren> = ({ children }) => {
   const { setIsLogin, userData, setUserData } = useGlobalStore();
 
+  const handleSetUserData = async (): Promise<boolean> => {
+    try {
+      const userInfo = await getUserInfo();
+      setUserData(userInfo);
+
+      return true;
+    } catch (error) {
+      localStorage.removeItem('token');
+      setUserData(null);
+
+      return false;
+    }
+  };
+
   const handleSetIsLogin = async () => {
     if (typeof window === 'undefined') {
       return;
     }
-
-    if (!!localStorage.getItem('token')) {
-      if (!userData) {
-        const userInfo = await getUserInfo();
-        setUserData(userInfo);
-      }
-
-      setIsLogin(true);
-    } else {
+    // token이 없으면 로그아웃 처리
+    if (!localStorage.getItem('token')) {
       setUserData(null);
       setIsLogin(false);
+
+      return;
     }
+
+    // token, userData가 있으면 로그인 처리
+    if (!!userData) {
+      setIsLogin(true);
+      return;
+    }
+
+    // token이 있고 userData가 없으면 userData를 가져와서 로그인 처리
+    const isSuccess = await handleSetUserData();
+    setIsLogin(isSuccess);
   };
 
   useEffect(() => {

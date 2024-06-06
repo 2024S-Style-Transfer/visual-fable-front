3b7b0379e34a213363e1168933e741d21c4d3ad3
Climier-code <ikowhddus@naver.com>
2024-06-06 22:50:41 +0900

    fix: isPageEnd State를 useRef로 수정

--- src/components/pages/Storage.tsx
+++ src/components/pages/Storage.tsx
@@ -6,7 +6,7 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import useGlobalStore from '@/store/globalStore';
 import { getBase64ImageUrlWithPrefix } from '@/utils/getBase64ImageUrlWithPrefix';
 import { PROJECT_REQ_SIZE } from '@/constants/generate';
@@ -38,18 +38,18 @@ const Storage: React.FC = () => {
   const router = useRouter();
   const [projectList, setProjectList] = useState<ProjectContent[]>([]);
   const [projectPage, setProjectPage] = useState(0);
-  const [isPageEnd, setIsPageEnd] = useState(false);
+  const isPageEnd = useRef(false);
 
   const loadProjectList = useCallback(async () => {
-    if (!isGlobalLoading && !isPageEnd) {
+    if (!isGlobalLoading && !isPageEnd.current) {
       try {
         setIsGlobalLoading(true);
         const projectResponse = await getUserProjectList(projectPage, PROJECT_REQ_SIZE);
         setProjectPage((prevProjectPage) => prevProjectPage + 1);
-        
+
         setProjectList((prevProjectList) => [...prevProjectList, ...projectResponse.content]);
-        if (projectResponse.last===true || projectResponse.numberOfElements < PROJECT_REQ_SIZE) {
-          setIsPageEnd(true);
+        if (projectResponse.last === true || projectResponse.numberOfElements < PROJECT_REQ_SIZE) {
+          isPageEnd.current = true;
         }
       } catch (error) {
         console.error(error);
@@ -57,21 +57,19 @@ const Storage: React.FC = () => {
         setIsGlobalLoading(false);
       }
     }
-  }, [isGlobalLoading, projectPage, isPageEnd]);
-
-  
+  }, [isGlobalLoading, projectPage, isPageEnd.current]);
 
   const onIntersect: IntersectionObserverCallback = useCallback(
     async ([entry], observer) => {
-      if (entry.isIntersecting && !isPageEnd) {
+      if (entry.isIntersecting && !isPageEnd.current) {
         observer.unobserve(entry.target);
         await loadProjectList();
         observer.observe(entry.target);
-      } else if (isPageEnd) {
+      } else if (isPageEnd.current) {
         observer.disconnect();
       }
     },
-    [loadProjectList, isPageEnd]
+    [loadProjectList, isPageEnd.current]
   );
 
   const { setTarget } = useIntersectionObserver({
@@ -90,7 +88,6 @@ const Storage: React.FC = () => {
     //   alert('로그인이 필요합니다.');
     //   window.location.href = '/';
     // }
-
     //loadProjectList();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -110,7 +107,7 @@ const Storage: React.FC = () => {
       <StorageDataWrapper>
         {projectList.map((project) => (
           <StorageItem key={project.projectId} onClick={() => handleClickStorageItem(project.projectId)}>
-              <Image
+            <Image
               src={getBase64ImageUrlWithPrefix(project.generatedItems[0].generatedImage)}
               alt={`projectExampleImage${project.projectId}`}
               width={168}
@@ -120,7 +117,7 @@ const Storage: React.FC = () => {
             <ItemText>{project.generatedItems[0].promptText}</ItemText>
           </StorageItem>
         ))}
-        <div ref ={setTarget}/>
+        <div ref={setTarget} />
       </StorageDataWrapper>
     </>
   );

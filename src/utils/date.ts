023378bfa714832d00c6053f215a23c80379e7ef
Climier-code <ikowhddus@naver.com>
2024-06-06 22:38:08 +0900

    fix: date format 디자인과 같이 수정

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,7 +3,9 @@ export const formatDate = (dateString: string) => {
 
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
+  const formatMonth = month < 10 ? `0${month}` : month;
   const day = date.getDate();
+  const formatDay = day < 10 ? `0${day}` : day;
 
-  return `${year}-${month}-${day}`;
+  return `${year}.${formatMonth}.${formatDay}`;
 };
